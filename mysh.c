e1b7e661c7167ea2115760a913135ba1c8ad7bf0
Zachary York <zyork@galapagos-22.cs.wisc.edu>
2014-09-28 19:34:27 -0500

    Added Program Run Code

--- mysh.c
+++ mysh.c
@@ -1,12 +1,11 @@
 
-//#include "types.h"
-//#include "stat.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-//#include "user.h"
+#include <unistd.h>
 
 #define MAX_ARGSIZE 1025
+const char* SYSTEM_CALLS[] = {"exit", "cd", "pwd"}; 
 
 int
 main(int argc, char *argv[])
@@ -20,19 +19,57 @@ main(int argc, char *argv[])
     while(1 == 1)
     {
     	count = 0;
+	char* cmd;
 
 	if (fgets(str, MAX_ARGSIZE, stdin) == NULL) {
 	    printf("Error!\n");
 	    continue;
         }
 	
-        arg = strtok(str, " \t");
-	while (arg != NULL && strcmp(arg, "\n") != 0) 
+        arg = strtok(str, " \t\n");
+	while (arg != NULL)
 	{
-                cmdargs[count] = arg;
-		count++;
-
-            arg = strtok(NULL, " \t");
+            cmdargs[count] = arg;
+            count++;
+		
+            arg = strtok(NULL, " \t\n");
 	}
+	cmd = cmdargs[0];
+       
+	if (strcmp(cmd, SYSTEM_CALLS[0]) == 0) 
+	{
+            exit(0);
+	}
+	else if (strcmp(cmd, SYSTEM_CALLS[1]) == 0) 
+	{
+            if (count == 1)
+	    {
+                chdir(getenv("HOME"));
+	    }
+	    else if (count == 2)
+	    {
+	        chdir(cmdargs[1]);
+	    }
+	    else 
+	    {
+	        printf("Error!\n");
+		continue;
+	    }
+	    continue;
+	}
+	else if (strcmp(cmd, SYSTEM_CALLS[2]) == 0) 
+	{
+	    if (count != 1)
+	    {
+	        printf("Error!\n");
+		continue;
+	    }
+	    printf("%s\n", getcwd(NULL, 0));
+	    continue;
+	}
+
+
+	    
     }
+    return 0;
 }
--- user/addnum.c
+++
@@ -1,9 +0,0 @@
-#include <syscall.c>
-
-void add(int x) {
-    int y= 0;
-
-    y = addnum(x);
-    printf("%d\n", y);
-
-}
